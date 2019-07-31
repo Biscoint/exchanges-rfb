2d1cad3e331ba55f39ea72f2389cf81f4ad6dd01
Augusto Resende <augusto.resende@biscoint.io>
2019-07-31 16:42:41 -0300

    test comments

--- examples/test.js
+++ examples/test.js
@@ -1,29 +1,29 @@
 var TestEx = require('../index.js');
-var testEx = new TestEx({
-    exchange_name: 'TestEx',
-    exchange_cnpj: '17.869.530/0001-73',
-    exchange_url: 'https://testex.example.com'
+var biscointTestex = new TestEx({
+    exchange_name: 'BiscointTestex', // Exchange Name
+    exchange_cnpj: '17.869.530/0001-73', // Exchange CNPJ
+    exchange_url: 'https://testex.biscoint.io' // Exchange URL
 });
 
-testEx.addBuySellOperation({ 
-    date: new Date(2019, 5, 25),
-    id: 'a12345',
-    brl_value: 'R$ 1500,80',
-    brl_fees: 'R$ 1,49',
-    coin_symbol: 'BTC',
-    coin_quantity: '0.0000001',
+biscointTestex.addBuySellOperation({ 
+    date: new Date(2019, 5, 25), // Input date javascript object
+    id: 'a12345', // Operation Id in your exchange
+    brl_value: 'R$ 1500,80', // BRL value trade (don't allow thousands separator)
+    brl_fees: 'R$ 1,49', // BRL fee trade (don't allow thousands separator)
+    coin_symbol: 'BTC', // CRYPTO symbol (BTC for Bitcoin, ETH for Ethereum)
+    coin_quantity: '0.0000001', // CRYPTO quantity (don't allow thousands separator)
     
-    buyer_id_type: 'CPF',
-    buyer_country: 'BR',
-    buyer_document: '442.467.420-74',
-    buyer_fullname: 'NOME COMPLETO',
-    buyer_address: 'Rua Nao Existente QD 0 LT 0',
+    buyer_id_type: 'CPF', // Document Type. Allowed: CPF, CNPJ, NIF_PF, NIF_PJ, PASSPORT, COUNTRY_NO_ID and USER_NO_ID
+    buyer_country: 'BR', // Country code
+    buyer_document: '442.467.420-74', // Document number
+    buyer_fullname: 'NOME COMPLETO', // Full name
+    buyer_address: 'Rua Nao Existente QD 0 LT 0', // Full address
 
-    seller_id_type: 'CPF',
-    seller_country: 'BR',
-    seller_document: '438.089.600-51',
-    seller_fullname: 'NOME COMPLETO',
-    seller_address: 'Rua Nao Existente QD 0 LT 0',
+    seller_id_type: 'CPF', // Document Type. Allowed: CPF, CNPJ, NIF_PF, NIF_PJ, PASSPORT, COUNTRY_NO_ID and USER_NO_ID
+    seller_country: 'BR', // Country code
+    seller_document: '438.089.600-51', // Document number
+    seller_fullname: 'NOME COMPLETO', // Full name
+    seller_address: 'Rua Nao Existente QD 0 LT 0', // Full address
 });
 
-console.log(testEx.exportFile());
+console.log(biscointTestex.exportFile());
